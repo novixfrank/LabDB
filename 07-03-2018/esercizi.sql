d8bb45f2652ead2ff393a29f57f6caa9f0dc4593
Gennaro Landolfi <gennarolandolfi@codedwork.it>
2018-03-14 21:44:23 +0100

    Aggiornata numerazione degli esercizi conforme alla traccia

--- 07-03-2018/esercizi.sql
+++ 07-03-2018/esercizi.sql
@@ -1,6 +1,6 @@
 -- Si consiglia la visione del file 'miglioramenti.sql'.
 
--- 1. Creazione della tabella
+-- 1.1 Creazione della tabella
 CREATE TABLE impiegato (
     cod NUMBER(5) CONSTRAINT impiegato_pk PRIMARY KEY,
     nome VARCHAR2(32) NOT NULL,
@@ -11,9 +11,7 @@ CREATE TABLE impiegato (
     CONSTRAINT impiegato_fk FOREIGN KEY (capo) REFERENCES impiegato(cod)
 );
 
-
-
--- 2. Inserimento
+-- 1.2. Inserimento
 INSERT INTO impiegato(cod, nome, cognome, data_assunto, stipendio, capo) VALUES (1, 'Alessandro', 'Rossi', TO_DATE('01/03/2014','DD/MM/YYYY'), 3000, NULL);
 INSERT INTO impiegato(cod, nome, cognome, data_assunto, stipendio, capo) VALUES (2, 'Alberto', 'Bianchi', TO_DATE('01/02/2013', 'DD/MM/YYYY'), 4000, NULL);
 INSERT INTO impiegato(cod, nome, cognome, data_assunto, stipendio, capo) VALUES (3, 'Andrea', 'Verdi', TO_DATE('03/06/2011', 'DD/MM/YYYY'), 5000, NULL);
@@ -27,7 +25,7 @@ UPDATE impiegato SET capo = 3 WHERE cod = 2;
 
 
 
--- 3. Dichiarazione della funzione di generazione delle e-mail:
+-- 2.1. Dichiarazione della funzione di generazione delle e-mail:
 CREATE OR REPLACE FUNCTION genera_mail(
     nome VARCHAR2,
     cognome VARCHAR2
@@ -41,7 +39,5 @@ END;
 -- Selezione dei dati:
 SELECT i.cod, genera_mail(i.nome, i.cognome) AS mail FROM impiegato i WHERE i.stipendio <= 3000;
 
-
-
--- 4. Selezione dei dipendenti assunti da più di 50 mesi sottoposti di 'Bianchi'
+-- 2.2. Selezione dei dipendenti assunti da più di 50 mesi sottoposti di 'Bianchi'
 SELECT * FROM impiegato i WHERE i.capo IN (SELECT cod FROM impiegato WHERE cognome = 'Bianchi') AND i.data_assunto <= ADD_MONTHS(CURRENT_DATE, -50);
