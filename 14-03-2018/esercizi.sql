1e6d83ef521b5a2f7e691b7bf9b0e90f16bc33b7
novixfrank <37139921+novixfrank@users.noreply.github.com>
2018-03-14 12:53:05 +0100

    Improvement del codice

--- 14-03-2018/esercizi.sql
+++ 14-03-2018/esercizi.sql
@@ -5,7 +5,7 @@ CREATE OR REPLACE VIEW stip_proposto (
     WHEN EXTRACT(YEAR FROM i.data_assunto) <= '2012' THEN i.stipendio * 1.15
     WHEN EXTRACT(YEAR FROM i.data_assunto) = '2013' THEN i.stipendio * 1.10
 ELSE i.stipendio
-END, NVL2(c.cognome, c.cognome, 'Nessuno')
+END, NVL(c.cognome, 'Nessuno')
 FROM impiegato i LEFT OUTER JOIN impiegato c ON i.capo = c.cod
 ORDER BY i.cod;
 
