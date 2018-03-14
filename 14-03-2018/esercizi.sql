fb6c5dae446ed594e3c126a0c045a7a8c5dcd494
Gennaro Landolfi <gennarolandolfi@codedwork.it>
2018-03-14 21:34:28 +0100

    Update esercizi.sql

--- 14-03-2018/esercizi.sql
+++ 14-03-2018/esercizi.sql
@@ -1,15 +1,15 @@
--- 1.1. Creazione della vista
+-- 1.1. Creazione della vista:
 CREATE OR REPLACE VIEW stip_proposto (
     cod, nome, attuale, proposto, capo
 ) AS SELECT i.cod, i.nome || ' ' || i.cognome, i.stipendio, CASE
     WHEN EXTRACT(YEAR FROM i.data_assunto) <= '2012' THEN i.stipendio * 1.15
     WHEN EXTRACT(YEAR FROM i.data_assunto) = '2013' THEN i.stipendio * 1.10
-ELSE i.stipendio
+    ELSE i.stipendio
 END, NVL(c.cognome, 'Nessuno')
 FROM impiegato i LEFT OUTER JOIN impiegato c ON i.capo = c.cod
 ORDER BY i.cod;
 
--- 1.2. Aggiornamento stipendio e cancellazione vista
+-- 1.2. Aggiornamento stipendio e cancellazione vista:
 UPDATE impiegato i
 SET i.stipendio = (
     SELECT s.proposto
@@ -19,16 +19,22 @@ SET i.stipendio = (
 
 DROP VIEW stip_proposto;
 
--- Creazione tabella 'personale' per esercizio 2 (Vedi file personale.sql)
 
--- 2.1. Inserimento "nuovi" impiegati da personale
+
+-- Creazione tabella 'personale' per esercizio 2 (Vedi file personale.sql);
+
+
+
+-- 2.1. Inserimento "nuovi" impiegati da personale:
 INSERT INTO impiegato i (
     SELECT p.cod_i AS cod, p.nome, p.cognome, p.data_assunto, p.stipendio, p.capo
     FROM personale p
 );
 
--- 2.2. Eliminazione impiegati assunti da meno di 3 anni
+-- 2.2. Eliminazione impiegati assunti da meno di 3 anni:
 DELETE FROM impiegato i WHERE ADD_MONTHS(i.data_assunto, 12 * 3) >= CURRENT_DATE;
 
--- 3. Selezione degli impiegati con doppia nel cognome
+
+
+-- 3. Selezione degli impiegati con doppia nel cognome:
 SELECT i.cod, i.cognome FROM impiegato i WHERE REGEXP_LIKE(LOWER(i.cognome), '([a-z])\1');
