b59f089534c9a4f2d09c319d56468373d8ade582
Gennaro Landolfi <gennarolandolfi@codedwork.it>
2018-03-14 21:49:38 +0100

    Aggiornata numerazione degli esercizi conforme alla traccia

--- 07-03-2018/miglioramenti.sql
+++ 07-03-2018/miglioramenti.sql
@@ -1,4 +1,4 @@
--- 3. Dichiarazione della funzione di generazione delle e-mail:
+-- 2.1 Dichiarazione della funzione di generazione delle e-mail:
 CREATE OR REPLACE FUNCTION genera_mail(
     nome VARCHAR2,
     cognome VARCHAR2
@@ -11,5 +11,5 @@ END;
 
 
 
--- 4. Selezione dei dipendenti assunti da più di 50 mesi sottoposti di 'Bianchi'
+-- 2.2 Selezione dei dipendenti assunti da più di 50 mesi sottoposti di 'Bianchi':
 SELECT i.* FROM impiegato i JOIN impiegato c ON i.capo = c.cod WHERE c.cognome = 'Bianchi' AND MONTHS_BETWEEN(CURRENT_DATE, i.data_assunto) >= 50;
