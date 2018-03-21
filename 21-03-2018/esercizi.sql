71df1374dce9f7bfce2d67d0103cff9ac8eb7c4b
Francesco Noviello <francesco.noviello97@gmail.com>
2018-03-21 11:54:19 +0100

    Esercizio 1.2 con alternativa

--- 21-03-2018/esercizi.sql
+++ 21-03-2018/esercizi.sql
@@ -1,4 +1,4 @@
--- 1.1 creazione vista e proiezione:
+-- 1.1. Creazione vista e proiezione:
 CREATE OR REPLACE VIEW stip_capo (
     cod_capo, stip_min, stip_max
 ) AS SELECT i.capo, MIN(i.stipendio), MAX(i.stipendio)
@@ -8,3 +8,16 @@ GROUP BY i.capo
 ORDER BY i.capo;
 
 -- SELECT * FROM stip_capo;
+
+-- 1.2. Calcolo stipendio medio dip. di Bianchi
+SELECT AVG(i.stipendio) AS stip_medio
+FROM impiegato i
+CONNECT BY i.capo = PRIOR i.cod
+START WITH i.capo IN (SELECT cod FROM impiegato WHERE cognome = 'Bianchi');
+
+-- Alternativa:
+SELECT AVG(i.stipendio) AS stip_medio
+FROM impiegato i
+WHERE LEVEL > 1
+CONNECT BY i.capo = PRIOR i.cod
+START WITH i.cod IN (SELECT cod FROM impiegato WHERE cognome = 'Bianchi');
