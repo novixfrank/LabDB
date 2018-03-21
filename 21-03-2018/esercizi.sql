0dab28b0b334988fac3b50b348ae3dc7ae6b02e3
Francesco Noviello <francesco.noviello97@gmail.com>
2018-03-21 12:11:47 +0100

    Esercizio 2

--- 21-03-2018/esercizi.sql
+++ 21-03-2018/esercizi.sql
@@ -9,7 +9,7 @@ ORDER BY i.capo;
 
 -- SELECT * FROM stip_capo;
 
--- 1.2. Calcolo stipendio medio dip. di Bianchi
+-- 1.2. Calcolo stipendio medio dip. di Bianchi:
 SELECT AVG(i.stipendio) AS stip_medio
 FROM impiegato i
 CONNECT BY i.capo = PRIOR i.cod
@@ -21,3 +21,17 @@ FROM impiegato i
 WHERE LEVEL > 1
 CONNECT BY i.capo = PRIOR i.cod
 START WITH i.cod IN (SELECT cod FROM impiegato WHERE cognome = 'Bianchi');
+
+-- 2. Stampa a video degli impiegati con codice compreso 7 e 10:
+DECLARE
+    cognomi VARCHAR2(500) := NULL;
+    cognome VARCHAR2(32) := NULL;
+BEGIN
+    FOR i IN 7..10 LOOP
+        SELECT imp.cognome INTO cognome FROM impiegato imp WHERE imp.cod = i;
+        cognomi := cognomi || ', ' || cognome;
+    END LOOP;
+
+    cognomi := LTRIM(cognomi, ', ');
+    DBMS_OUTPUT.PUT_LINE(cognomi);
+END;
