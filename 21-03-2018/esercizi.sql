fde1523776c7b6e32b4dc8eb0239a3aa3928d00a
Francesco Noviello <francesco.noviello97@gmail.com>
2018-03-21 11:03:24 +0100

    Esercizio 1.1

--- 21-03-2018/esercizi.sql
+++ 21-03-2018/esercizi.sql
@@ -1,1 +1,10 @@
--- TODO Lezione del giorno;
+-- 1.1 creazione vista e proiezione:
+CREATE OR REPLACE VIEW stip_capo (
+    cod_capo, stip_min, stip_max
+) AS SELECT i.capo, MIN(i.stipendio), MAX(i.stipendio)
+FROM impiegato i
+WHERE i.capo IS NOT NULL
+GROUP BY i.capo
+ORDER BY i.capo;
+
+-- SELECT * FROM stip_capo;
