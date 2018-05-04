f0d383f37b4a72148cc6bdc19b7fc1512264b512
Gennaro Landolfi <rinodrummer@yahoo.it>
2018-05-04 11:31:26 +0200

    Migliorato il codice

--- 05-02-2018/src/io/github/labdb/Applicazione.java
+++ 05-02-2018/src/io/github/labdb/Applicazione.java
@@ -3,20 +3,20 @@ package io.github.labdb;
 public class Applicazione {
     public static void main(String[] args) {
         Articolo a = null;
-        Videogame v = null;
 
         if (args.length > 0) {
             if (args[0].equals("Articolo") && args.length == 2) {
                 a = new Articolo(Integer.parseInt(args[1]));
-                System.out.println(a.toString());
             }
             else if (args[0].equals("Videogame") && args.length == 3) {
-                v = new Videogame(Integer.parseInt(args[1]), args[2]);
-                System.out.println(v.toString());
+                a = new Videogame(Integer.parseInt(args[1]), args[2]);
             }
             else {
                 System.out.println("Errore: il numero di argomenti indicati non corrisponde a quelli richiesti.");
+                return;
             }
+            
+            System.out.println(a.toString());
         }
         else {
             System.out.println("Errore: nessun elemento specificato.");
