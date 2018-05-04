a0920dee9f444526a1bd703da89afa6583bafc1a
Gennaro Landolfi <rinodrummer@yahoo.it>
2018-05-04 11:31:04 +0200

    Aggiornati i comandi di esecuzione

--- 05-02-2018/run.sh
+++ 05-02-2018/run.sh
@@ -1,16 +1,16 @@
-javac ./src/io/github/labdb/*.java -d ./bin
+javac -d ./bin ./src/io/github/labdb/*.java
 
 # Terminazione con errore: "Errore: nessun elemento specificato."
-java io.github.labdb.Applicazione
+java -cp ./bin io.github.labdb.Applicazione
 
 # Terminazione con errore: "Errore: il numero di argomenti indicati non corrisponde a quelli richiesti.
-java io.github.labdb.Applicazione Articolo 1 Test
+java -cp ./bin io.github.labdb.Applicazione Articolo 1 Test
 
 # Terminazione con errore: "Errore: il numero di argomenti indicati non corrisponde a quelli richiesti.
-java io.github.labdb.Applicazione Videogame 1
+java -cp ./bin io.github.labdb.Applicazione Videogame 1
 
 # Terminazione con successo: "Articolo: 1;"
-java io.github.labdb.Applicazione Articolo 1
+java -cp ./bin io.github.labdb.Applicazione Articolo 1
 
 # Terminazione con successo: "Videogame: Codice: 1; Titolo: Test;"
-java io.github.labdb.Applicazione Videogame 1 Test
+java -cp ./bin io.github.labdb.Applicazione Videogame 1 Test
