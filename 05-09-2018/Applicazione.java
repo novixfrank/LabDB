2bcf6d454dc4f221133fa86533fb3aceff80568b
Francesco Noviello <francesco.noviello97@gmail.com>
2018-05-09 13:03:18 +0200

    Iniziata risoluzione dell'esercizio.

---
+++ 05-09-2018/Applicazione.java
@@ -0,0 +1,19 @@
+package io.github.labd;
+
+
+public class Applicazione {
+  public static void main(String[] args) {
+    try{
+      NumeroComplesso z = null;
+
+      if(args.length == 2){
+        //chiamare costruttore
+        //stampare il numero complesso,il modulo, le coordinate polari
+      }
+    }
+    catch (NumberFormatException){
+      //scrivi "numero scritto male" e chiudi programma
+     }
+  }
+
+}
