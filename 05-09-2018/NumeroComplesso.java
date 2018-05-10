a385c45e88316806e8f707bba783e4607c240762
Gennaro Landolfi <rinodrummer@yahoo.it>
2018-05-10 08:12:58 +0200

    Aggiornata sintassi in conformità con lo stile standard di Java

--- 05-09-2018/NumeroComplesso.java
+++ 05-09-2018/NumeroComplesso.java
@@ -1,54 +1,54 @@
 package io.github.labd;
+
 import java.lang.Math;
 
-public class NumeroComplesso extends Number{
+public class NumeroComplesso extends Number {
 
-  private double real=0.0d;
-  private double imaginary=0.0d;
+    private double real = 0.0d;
+    private double imaginary = 0.0d;
 
-  public NumeroComplesso(double rp, double ip){
-    real=rp;
-    imaginary=ip;
-  }
+    public NumeroComplesso(double rp, double ip) {
+        real = rp;
+        imaginary = ip;
+    }
 
-  public setNumero(double rp, double ip){
-    real=rp;
-    imaginary=ip;
-  }
+    public setNumero(double rp, double ip) {
+        real = rp;
+        imaginary = ip;
+    }
 
-  public string getNumero(){
-    return real + "+" + imaginary +"*i";
-  }
+    public string getNumero() {
+        return real + " + " + imaginary + " * i";
+    }
 
-  public double getModulo(){
-    double x=0.0d;
-    x=(Math.pow(real, 2))+(Math.pow(imaginary, 2));
+    public double getModulo() {
+        double x = 0.0d;
+        x = (Math.pow(real, 2)) + (Math.pow(imaginary, 2));
 
-    return Math.sqrt(x);
-  }
+        return Math.sqrt(x);
+    }
 
-  public int intValue(){
-    return (int)getModulo();
-  }
+    public int intValue() {
+        return (int) getModulo();
+    }
 
-  public long longValue(){
-    return (long)getModulo();
-  }
+    public long longValue() {
+        return (long) getModulo();
+    }
 
-  public float floatValue(){
-    return (float)getModulo();
-  }
+    public float floatValue() {
+        return (float) getModulo();
+    }
 
-  public double doubleValue(){
-    return (double)getModulo();
-  }
+    public double doubleValue() {
+        return (double) getModulo();
+    }
 
-  public double args(){
-    return Math.atan2(imaginary, real);
-  }
+    public double args() {
+        return Math.atan2(imaginary, real);
+    }
 
-  public string polarCoordinate(){
-    return "(" + getModulo + "," + args() + ")";
-  }
-  
+    public string polarCoordinate() {
+        return "(" + getModulo + "," + args() + ")";
+    }
 }
