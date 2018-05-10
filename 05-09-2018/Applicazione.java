5c5dd377153e273ab7ab49f82fd61b96a4931bfb
Gennaro Landolfi <rinodrummer@yahoo.it>
2018-05-10 08:15:49 +0200

    Aggiornata sintassi in conformità con lo stile standard di Java

--- 05-09-2018/Applicazione.java
+++ 05-09-2018/Applicazione.java
@@ -1,19 +1,15 @@
-package io.github.labd;
-
-
 public class Applicazione {
-  public static void main(String[] args) {
-    try{
-      NumeroComplesso z = null;
+    public static void main(String[] args) {
+        try {
+            NumeroComplesso z = null;
 
-      if(args.length == 2){
-        //chiamare costruttore
-        //stampare il numero complesso,il modulo, le coordinate polari
-      }
+            if(args.length == 2) {
+                //chiamare costruttore
+                //stampare il numero complesso, il modulo, le coordinate polari
+            }
+        }
+        catch (NumberFormatException) {
+            //scrivi "numero scritto male" e chiudi programma
+        }
     }
-    catch (NumberFormatException){
-      //scrivi "numero scritto male" e chiudi programma
-     }
-  }
-
 }
