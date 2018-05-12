d076a0c1c7d8677b63b6aea0b098b30ef2ba43ef
Gennaro Landolfi <rinodrummer@yahoo.it>
2018-05-12 17:15:16 +0200

    Corretto esercizio

--- 05-09-2018/src/io/github/labdb/Applicazione.java
+++ 05-09-2018/src/io/github/labdb/Applicazione.java
@@ -3,20 +3,20 @@ package io.github.labdb;
 public class Applicazione {
     public static void main(String[] args) {
         try {
-            //creazione con costruttore e argomenti null
-            NumeroComplesso z = new NumeroComplesso(null,null);
-
             if(args.length == 2) {
-                //settare il numero
-                z = setNumero(args[0], args[1]);
+                NumeroComplesso z = new NumeroComplesso(args[0], args[1]);
+                
                 //stampare il numero complesso, il modulo, le coordinate polari
-                System.out.println("\n Numero complesso:" + getNumero);
-                System.out.println("\n Modulo numero:" + getModulo);
-                System.out.println("\n Coordinare polari:" + CoordinatePolari);
+                System.out.println("\n Numero complesso: " + z.getNumero());
+                System.out.println("\n Modulo numero: " + z.getModulo());
+                System.out.println("\n Coordinare polari: " + z.getCoordinatePolari());
+            }
+            else {
+                System.out.println("Errore: numero di argomenti non valido.");
             }
         }
         catch (NumberFormatException) {
-            System.out.println("Errore: formato numero non valido.");
+            System.out.println("Errore: formato numerico non valido.");
         }
     }
 }
